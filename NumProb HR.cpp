044428328337bf7d5ccc97e04690d99c7625ef66
thonmay <89507870+thonmay@users.noreply.github.com>
2023-10-16 15:26:49 +0200

    Add files via upload

---
+++ NumProb HR.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>> d;
+    cin>> e;
+
+    cout<< a<< endl;
+    cout<< b<< endl;
+    cout<< c<< endl;
+    cout<< d<< endl;
+    cout<< e<< endl;
+
+
+
+
+
+
+
+    return 0;
+}
